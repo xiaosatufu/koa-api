f554c8fe885a9b077414410d825666337023c8db
xiaosatufu@163.com <xiaosatufu@163.com>
2019-07-10 23:17:31 +0800

    使用Mongoose实现增删改查

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,34 +1,38 @@
-const db = [{ name: '李雷' }]
+
+const User = require('../models/users')
 class UsersCtl {
-    find(ctx) {
-        ctx.body = db
+    async find(ctx) {
+        ctx.body = await User.find()
     }
-    findById(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412)
-        }
-        ctx.body = db[ctx.params.id * 1]
+    async findById(ctx) {
+        const user = await User.findById(ctx.params.id)
+        if (!user) { ctx.throw(404) }
+        ctx.body = user
     }
-    create(ctx) {
+    async create(ctx) {
         ctx.verifyParams({
-            name: { type: 'string', required: true },
-            age: { type: 'number', required: false }
+            name: { type: 'string', required: true }
         });
-        db.push(ctx.request.body)
-        ctx.body = ctx.request.body
+        const user = await new User(ctx.request.body).save()
+        ctx.body = user
+
     }
-    update(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412)
+    async update(ctx) {
+        ctx.verifyParams({
+            name: { type: 'string', required: true }
+        });
+        const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+        if (!user) {
+            ctx.throw(404, '用户不存在')
+
         }
-        db[ctx.params.id * 1] = ctx.request.body
-        ctx.body = ctx.request.body
+        ctx.body = user
     }
-    delete(ctx) {
-        if (ctx.params.id * 1 >= db.length) {
-            ctx.throw(412)
+    async delete(ctx) {
+        const user = await User.findByIdAndRemove(ctx.params.id)
+        if (!user) {
+            ctx.throw(404, '用户不存在')
         }
-        db.splice(ctx.params.id * 1, 1)
         ctx.status = 204;
     }
 }
