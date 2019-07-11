7f7be36adb76e794cf1a165a58fab98875a836ea
xiaosatufu@163.com <xiaosatufu@163.com>
2019-07-11 23:00:52 +0800

    使用koa-jwt中间件使用用户认证与授权

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,4 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const router = new Router({ prefix: '/users' })
 const { secret } = require('../config')
@@ -6,20 +6,7 @@ const { secret } = require('../config')
 const { find, findById, create, update, delete: del, login, checkOwner } = require('../controllers/users')
 
 //鉴权
-const auth = async (ctx, next) => {
-    //获取anthorization
-    const { authorization = '' } = ctx.request.header;
-    //处理authorization 获取token
-    const token = authorization.replace('Bearer ', '')
-    //验证token
-    try {
-        const user = jsonwebtoken.verify(token, secret)
-        ctx.state.user = user
-    } catch (err) {
-        ctx.throw(401, err.message)
-    }
-    await next()
-}
+const auth = jwt({secret})
 
 console.log(find)
 router.get('/', find)
